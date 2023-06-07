278b230166b64e337fdf576e0178f51c2f6751de
Anna Pinchuk <76442355+annapinchuk@users.noreply.github.com>
2023-06-07 12:22:27 -0700

    done with haeder files

---
+++ sources/MagicalContainer.cpp
@@ -0,0 +1,9 @@
+#include "MagicalContainer.hpp"
+using namespace ariel;
+MagicalContainer::MagicalContainer():items(),p_prime(){}
+MagicalContainer::~MagicalContainer(){}
+size_t MagicalContainer::size(){return items.size();}
+void MagicalContainer::addElement(int element){
+}
+void MagicalContainer::removeElement(int element){
+}
